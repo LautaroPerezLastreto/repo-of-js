0644ffa743c5db33a6aa870ac828901fa1762708
Lautaro Perez Lastreto <lautaroperezlastreto@gmail.com>
2022-06-27 18:50:20 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let nombre = prompt("ingrese su nombre");
+alert("Bienvenido/a " + nombre )
+
+let edadNino = 7;
+let edadPadre = 18;
+let pagoEntrada = true;
+if (pagoEntrada == false && (edadNino >= 8 || edadPadre >= 30)){
+    console.log("Puede subir")
+}else{
+    console.log("No puede subir")
+}
+
+let resultado;
+for(let i = 1; i <= 50; i++){
+    resultado = i + i;
+    console.log(i + " + " + i + " es igual a " + resultado)
+}  
+
+let resultados;
+let numero = 3;
+for (let i = 0; i <= 2; i++) {
+    prompt(resultados = numero * i)
+}
+
+for (let i = 0; i <= 3; i++) {
+    prompt("Hola")
+}
+
+let entrada = prompt("Escriba que piensa");
+while (entrada != "ESC"){
+    alert("El usuario piensa que " + entrada);
+    entrada = prompt("Ingrese otro pensamiento")
+    alert("Para salir de esto escriba ESC, si quiere proseguir siga escribiendo")
+}
+
+let noRepetir = false;
+let repetir = true;
+do{
+    console.log("No se tilda la pc");
+}while(noRepetir)
